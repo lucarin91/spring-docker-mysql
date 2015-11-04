04826b945801b9cc66a14f2366e368d97a287b55
lucarin91 <lucarin91@gmail.com>
2015-11-04 02:47:56 +0100

    init

---
+++ src/main/java/com/lucarin/DemoApplication.java
@@ -0,0 +1,79 @@
+package com.lucarin;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    @Autowired
+    private PersonRepository repository;
+
+    @RequestMapping("/")
+    public String home() {
+        Person n = new Person();
+        n.setFirstName("DIO");
+        n.setLastName("eDai");
+
+        this.repository.save(n);
+
+        String ris="";
+        for(Person p : this.repository.findAll()){
+            ris+=p.getName()+"</br>";
+        };
+        return ris;
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+@Repository
+interface PersonRepository extends CrudRepository<Person, Long> {
+
+}
+
+@Entity
+class Person {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastname) {
+        this.lastName = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [firstName=" + this.firstName + ", lastName=" + this.lastName
+                + "]";
+    }
+
+    public String getName() {
+        return this.firstName + " " + this.lastName;
+    }
+}
---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+baseName="springapp"
+
+docker run --name ${baseName}-data spring/${baseName}-data
+
+docker stop ${baseName}-mysql
+docker rm ${baseName}-mysql
+docker run --name ${baseName}-mysql \
+              --volumes-from ${baseName}-data \
+              -p 3306:3306 \
+              -d \
+              spring/${baseName}-mysql
+
+docker stop ${baseName}-web
+docker rm ${baseName}-web
+docker run --name ${baseName}-web \
+              -p 8080:8080 \
+              --link ${baseName}-mysql:mysql \
+              -t \
+              spring/${baseName}-web
