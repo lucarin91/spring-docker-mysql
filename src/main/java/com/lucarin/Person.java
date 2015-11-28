5c51f401699bc9b4242dcc431e5b4d96b6326ca5
lucarin91 <lucarin91@gmail.com>
2015-11-28 13:39:33 +0100

    split file and fix problems

--- src/main/java/com/lucarin/Person.java
+++ src/main/java/com/lucarin/Person.java
@@ -1,5 +1,11 @@
 package com.lucarin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
